22b46f25f7a15da5d94b0344cb92c9859e3b9bbd
JXBXBXXY <ifyoudanila@gmail.com>
2025-09-06 05:14:56 -0700

    Update env.js

--- FroggyHub/env.js
+++ FroggyHub/env.js
@@ -1,5 +1,5 @@
 // env.js (public, safe: anon key + URL)
 window.ENV = {
-  PUBLIC_SUPABASE_URL: "https://mefprtvuwxbqvpjadgkq.supabase.co",
-  PUBLIC_SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiAic3VwYWJhc2UiLCAicmVmIjogIm1lZnBydHZ1d3hicXZwamFkZ2txIiwgInJvbGUiOiAiYW5vbiIsICJpYXQiOiAwLCAiZXhwIjogOTk5OTk5OTk5OX0.c2ln"
+  PUBLIC_SUPABASE_URL: "https://smamhlfzerjkdfhtwhdv.supabase.co",
+  PUBLIC_SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNtYW1obGZ6ZXJqa2RmaHR3aGR2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUxMzQ0MzYsImV4cCI6MjA3MDcxMDQzNn0.PwRF3OAtlpJ7zu2lsIb46V7XLINlyhfC97Jgbu--Vv4"
 };
